98cb601c0d0dfe85a800193fd842dd52c1af6ab9
Jamehop <Jamehop@DESKTOP-DRQLDNL>
2021-06-16 11:53:07 +0200

    Dale mas

--- Speedrun/src/test/java/Model/UnidadesEmergenciaBuilderTest.java
+++ Speedrun/src/test/java/Model/UnidadesEmergenciaBuilderTest.java
@@ -18,5 +18,62 @@ public class UnidadesEmergenciaBuilderTest {
 		}
 		return null;
 	}
+	
+	/*
+	 * @Test
+	public void comprobarDNIValido() {
+		try {
+			Assert.assertTrue(BuilderClienteDao.validarDocumento(misDocumentos.DNI, "12345678Q"));
+		} catch (MisExcepciones e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void comprobarDNINoValido() {
+		try {
+			Assert.assertFalse(BuilderClienteDao.validarDocumento(misDocumentos.DNI, "12334re5678Q"));
+		} catch (MisExcepciones e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void comprobarNIEValido() throws MisExcepciones {
+		try {
+			Assert.assertTrue(BuilderClienteDao.validarDocumento(misDocumentos.NIE, "Q2345678Q"));
+		} catch (MisExcepciones e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void comprobarNIENoValido() throws MisExcepciones {
+		try {
+			Assert.assertFalse(BuilderClienteDao.validarDocumento(misDocumentos.NIE, "123drfth45678Q"));
+		} catch (MisExcepciones e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void comprobarEmailValido() {
+		try {
+			Assert.assertTrue(BuilderClienteDao.validarEmail("albaricoque12@gmail.com"));
+		} catch (MisExcepciones e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void comprobarEmailNoValido() {
+		try {
+			Assert.assertFalse(BuilderClienteDao.validarEmail("emailerroneo"));
+		} catch (MisExcepciones e) {
+			e.printStackTrace();
+		}
+	}
+	 * 
+	 */
 
 }
